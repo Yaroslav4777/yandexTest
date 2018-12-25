932876cfe3f969ef4d14179412be2ee367dfc7cb
Yaroslav4777 <rdz234@mail.ru>
2018-12-26 02:22:12 +0700

    Initial commit

---
+++ src/test/java/ru/mail/rdz234/FirstTest.java
@@ -0,0 +1,56 @@
+package ru.mail.rdz234;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "/home/yaroslav/Work/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+        driver.get("https://yandex.ru/");
+
+    }
+
+    @Test
+
+    public void userLogin() {
+        WebElement firstButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div/div[1]/div/a[1]"));
+        firstButton.click();
+        WebElement loginField = driver.findElement(By.xpath("//*[@id='passp-field-login']"));
+        loginField.sendKeys("testuser47@yandex.ru");
+        WebElement enterButton = driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div/div[1]/form/div[3]/button[1]"));
+        enterButton.click();
+        WebElement passwordField = driver.findElement(By.xpath("//*[@id='passp-field-passwd']"));
+        passwordField.sendKeys("testpassworD");
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div/div/form/div[2]/button"));
+        loginButton.click();
+        WebElement profileUser = driver.findElement(By.xpath("//*[@id='nb-1']/body/div[2]/div[5]/div/div[2]/div[4]/div[7]/div"));
+        String mailUser = profileUser.getText();
+        Assert.assertEquals("testuser47", mailUser);
+    }
+
+    @AfterClass
+
+    public static void tearDown(){
+        WebElement menuUser = driver.findElement(By.xpath("//*[@id='recipient-1']"));
+        menuUser.click();
+        WebElement secondButton = driver.findElement(By.xpath("//*[@id='nb-4']/div/div/div[8]/a"));
+        secondButton.click();
+        driver.quit();
+    }
+}
